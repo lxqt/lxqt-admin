d7ab45d30833f17c1eb0294aff2581d974396589
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2014-05-21 10:04:24 +0800

    Initial commit.

---
+++ lxqt-admin-time/timeadmindialog.h
@@ -0,0 +1,42 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  <copyright holder> <email>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+#ifndef TIMEADMINDIALOG_H
+#define TIMEADMINDIALOG_H
+
+#include <QDialog>
+
+namespace Ui
+{
+class TimeAdminDialog;
+}
+
+class TimeAdminDialog : public QDialog
+{
+    Q_OBJECT
+public:
+  TimeAdminDialog();
+  ~TimeAdminDialog();
+
+private:
+    Ui::TimeAdminDialog* ui;
+};
+
+#endif // TIMEADMINDIALOG_H
