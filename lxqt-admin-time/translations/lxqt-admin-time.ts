33191591c9f0b5164bb007bdc68a18f87f3427f4
Luís Pereira <luis.artur.pereira@gmail.com>
2014-10-31 15:51:02 -0700

    updates Translations

---
+++ lxqt-admin-time/translations/lxqt-admin-time.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.ui" line="14"/>
+        <source>Time and Date Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="23"/>
+        <source>Set current date and time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="30"/>
+        <source>Time zone:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="40"/>
+        <source>Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="47"/>
+        <source>hh:mm:ss AP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="120"/>
+        <source>Authentication Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
