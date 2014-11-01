f182cb42f9133e611c8f1ae91f81459c3311b2f8
Ilya87 <yast4ik@yahoo.com>
2014-11-01 20:10:08 +0300

    Update translation a little more

---
+++ lxqt-admin-time/translations/lxqt-admin-time_ru.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.ui" line="14"/>
+        <source>Time and Date Settings</source>
+        <translation>Настройки даты и времени</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="23"/>
+        <source>Set current date and time</source>
+        <translation>Установить текущую дату и время</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="30"/>
+        <source>Time zone:</source>
+        <translation>Часовой пояс:</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="40"/>
+        <source>Time:</source>
+        <translation>Время:</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.ui" line="47"/>
+        <source>hh:mm:ss AP</source>
+        <translatorcomment>What is  AP?</translatorcomment>
+        <translation>чч:мм:сс AP</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="120"/>
+        <source>Authentication Error</source>
+        <translation>Ошибка аутентификации</translation>
+    </message>
+</context>
+</TS>
