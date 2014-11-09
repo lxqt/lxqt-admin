66a0d5a4344b992f49de0f31fb4ea417b0aac46d
Jakub Laník - PC <kubanecxxx@gmail.com>
2014-11-09 10:07:46 +0100

    Whole GUI of time/date setup has been rewritten from scratch the app has now style of LXQT config dialogs based on LxQt::ConfigDialog the time and date setting is divided into one configuration widget and timezone selection is divided into second config widget
    Licence added into source/header files

--- lxqt-admin-time/timeadmindialog.cpp
+++ lxqt-admin-time/timeadmindialog.cpp
@@ -1,75 +1,113 @@
-/*
- * <one line to give the library's name and an idea of what it does.>
- * Copyright (C) 2014  <copyright holder> <email>
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXQt - a lightweight, Qt based, desktop toolset
+ * http://lxqt.org
+ *
+ * Copyright: 2014 LXQt team
+ * Authors:
+ *   Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- */
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
 
 #include "timeadmindialog.h"
-#include <QFile>
-#include <QDebug>
+#include <QLabel>
+#include <QCloseEvent>
 #include <QMessageBox>
-#include <QTimer>
+#include <QDateTime>
+#include <QMap>
 
-#define ZONETAB_PATH     "/usr/share/zoneinfo/zone.tab"
+#include "datetime.h"
+#include "timezone.h"
 
-TimeAdminDialog::TimeAdminDialog():
-    QDialog(),
-    mChangeTime(false),
-    mTimeConfig(OOBS_TIME_CONFIG(oobs_time_config_get()))
+
+#define ZONETAB_PATH "/usr/share/zoneinfo/zone.tab"
+
+TimeAdminDialog::TimeAdminDialog(QWidget *parent):
+    LxQt::ConfigDialog(tr("Time and date configuration"),new LxQt::Settings("TimeDate"), parent),
+    mTimeConfig(OOBS_TIME_CONFIG(oobs_time_config_get())),
+    mUserLogedIn(false)
 {
-    ui.setupUi(this);
-
     oobs_object_update(OOBS_OBJECT(mTimeConfig));
-    loadTimeZones();
 
-    mTimer = new QTimer();
-    connect(mTimer, SIGNAL(timeout()), SLOT(updateTime()));
-    mTimer->start(1000);
-    updateTime();
-    ui.calendar->showToday();
+    setMinimumSize(QSize(400,400));
+    mWindowTitle = windowTitle();
 
-    connect(ui.time, SIGNAL(timeChanged(QTime)), SLOT(onTimeChanged()));
+
+    mDateTimeWidget = new DateTime(this);
+    addPage(mDateTimeWidget,tr("Date and time"));
+    connect(this,SIGNAL(reset()),mDateTimeWidget,SLOT(reload()));
+    connect(mDateTimeWidget,&DateTime::changed,this,&TimeAdminDialog::onChanged);
+    mDateTimeWidget->setProperty("pModified",M_TIMEDATE);
+
+    QStringList zones;
+    QString currentZone;
+    loadTimeZones(zones,currentZone);
+    mTimezoneWidget = new Timezone(zones,currentZone,this);
+    addPage(mTimezoneWidget,tr("Timezone"));
+    connect(this,&TimeAdminDialog::reset,mTimezoneWidget,&Timezone::reload);
+    connect(mTimezoneWidget,&Timezone::changed,this,&TimeAdminDialog::onChanged);
+    mTimezoneWidget->setProperty("pModified",M_TIMEZONE);
 }
 
 TimeAdminDialog::~TimeAdminDialog()
 {
     if(mTimeConfig)
         g_object_unref(mTimeConfig);
-    delete mTimer;
 }
 
-void TimeAdminDialog::onTimeChanged()
+void TimeAdminDialog::onChanged(bool ch)
 {
-    // stop the timer if the time is changed by the user
-    mTimer->stop();
-    mChangeTime = true;
+    widgets_modified_enum flag = (widgets_modified_enum)
+            sender()->property("pModified").toUInt();
+    ch ? mWidgetsModified |= flag : mWidgetsModified &= ~flag;
+    showChangedStar();
 }
 
-void TimeAdminDialog::updateTime()
+
+void TimeAdminDialog::showChangedStar()
 {
-    // do not emit timeChanged() signal
-    ui.time->blockSignals(true);
-    ui.time->setTime(QTime::currentTime());
-    ui.time->blockSignals(false);
+    if (mWidgetsModified)
+        setWindowTitle(mWindowTitle + "*");
+    else
+        setWindowTitle(mWindowTitle);
 }
 
-void TimeAdminDialog::loadTimeZones()
+void TimeAdminDialog::closeEvent(QCloseEvent *event)
 {
-    QStringList timeZones;
+    //save changes to system
+    if (mWidgetsModified)
+    {
+        if (logInUser())
+        {
+            saveChangesToSystem();
+            event->accept();
+        }
+        else
+        {
+            event->ignore();
+        }
+    }
+}
+
+void TimeAdminDialog::loadTimeZones(QStringList & timeZones, QString & currentTimezone)
+{
+    timeZones.clear();
     QFile file(ZONETAB_PATH);
     if(file.open(QIODevice::ReadOnly))
     {
@@ -86,40 +124,44 @@ void TimeAdminDialog::loadTimeZones()
         }
         file.close();
     }
-    timeZones.sort();
-    ui.timeZone->addItems(timeZones);
-    
-    int sel = -1;
-    const char* currentTimeZone = oobs_time_config_get_timezone(mTimeConfig);
-    if(currentTimeZone)
-        sel = timeZones.indexOf(QLatin1String(currentTimeZone));
-    ui.timeZone->setCurrentIndex(sel);
+    currentTimezone = QString::fromLatin1(oobs_time_config_get_timezone(mTimeConfig));
 }
 
-void TimeAdminDialog::accept()
+
+
+void TimeAdminDialog::saveChangesToSystem()
 {
-    // relly apply the settings
+    QByteArray timeZone = mTimezoneWidget->timezone().toLatin1();
+    // FIXME: currently timezone settings does not work. is this a bug of system-tools-backend?
+    if(!timeZone.isEmpty() && mWidgetsModified.testFlag(M_TIMEZONE))
+        oobs_time_config_set_timezone(mTimeConfig, timeZone.constData());
+
+    if(mWidgetsModified.testFlag(M_TIMEDATE))
+    {
+        QDate d = mDateTimeWidget->dateTime().date();
+        QTime t = mDateTimeWidget->dateTime().time();
+        // oobs seems to use 0 based month
+        oobs_time_config_set_time(mTimeConfig, d.year(), d.month() - 1, d.day(), t.hour(), t.minute(), t.second());
+    }
+    oobs_object_commit(OOBS_OBJECT(mTimeConfig));
+}
+
+bool TimeAdminDialog::logInUser()
+{
+    if (mUserLogedIn)
+        return true;
+
     GError* err = NULL;
     if(oobs_object_authenticate(OOBS_OBJECT(mTimeConfig), &err))
     {
-        QByteArray timeZone = ui.timeZone->currentText().toLatin1();
-        // FIXME: currently timezone settings does not work. is this a bug of system-tools-backend?
-        if(!timeZone.isEmpty())
-            oobs_time_config_set_timezone(mTimeConfig, timeZone.constData());
-        QDate d = ui.calendar->selectedDate();
-        if(mChangeTime)
-        {
-            QTime t = ui.time->time();
-            // oobs seesm to use 0 based month
-            oobs_time_config_set_time(mTimeConfig, d.year(), d.month() - 1, d.day(), t.hour(), t.minute(), t.second());
-        }
-        oobs_object_commit(OOBS_OBJECT(mTimeConfig));
+        mUserLogedIn = true;
+        return true;
     }
     else if(err)
     {
         QMessageBox::critical(this, tr("Authentication Error"), QString::fromUtf8(err->message));
         g_error_free(err);
     }
-    QDialog::accept();
-}
 
+    return false;
+}
---
+++ lxqt-admin-time/timezone.cpp
@@ -0,0 +1,88 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXQt - a lightweight, Qt based, desktop toolset
+ * http://lxqt.org
+ *
+ * Copyright: 2014 LXQt team
+ * Authors:
+ *   Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#include "timezone.h"
+#include "ui_timezone.h"
+
+Timezone::Timezone(const QStringList & zones, const QString & currentimezone, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Timezone),
+    mZoneChanged(false),
+    mZonesList(zones)
+{
+    ui->setupUi(this);
+
+    if (!currentimezone.isEmpty())
+        ui->label_timezone->setText(currentimezone);
+    else
+        ui->label_timezone->setText(tr("None"));
+
+    mZonesList.sort();
+    ui->list_zones->addItems(mZonesList);
+
+    reload();
+}
+
+Timezone::~Timezone()
+{
+    delete ui;
+}
+
+void Timezone::reload()
+{
+    ui->list_zones->setCurrentRow(-1);
+    mZoneChanged = false;
+    QList<QListWidgetItem *> list = ui->list_zones->findItems(ui->label_timezone->text(),Qt::MatchExactly);
+    if (list.count())
+        ui->list_zones->setCurrentItem(list.at(0));
+
+    emit changed(mZoneChanged);
+}
+
+QString Timezone::timezone() const
+{
+    if (ui->list_zones->currentItem())
+        return ui->list_zones->currentItem()->text();
+    else
+        return QString();
+}
+
+void Timezone::on_list_zones_itemActivated(QListWidgetItem * item)
+{
+    bool old = mZoneChanged;
+    mZoneChanged = item->text() != ui->label_timezone->text();
+
+    if (mZoneChanged != old)
+        emit changed(mZoneChanged);
+}
+
+void Timezone::on_edit_filter_textChanged(const QString &arg1)
+{
+    QRegExp reg(arg1, Qt::CaseInsensitive,QRegExp::Wildcard);
+    ui->list_zones->clear();
+    ui->list_zones->addItems(mZonesList.filter(reg));
+}
