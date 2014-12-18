22ba1dd0401aabafceb5a4f36be81a5f15e1754b
Luís Pereira <luis.artur.pereira@gmail.com>
2014-12-18 03:27:41 -0800

    Updates translations sources

--- lxqt-admin-time/translations/lxqt-admin-time.ts
+++ lxqt-admin-time/translations/lxqt-admin-time.ts
@@ -2,35 +2,96 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>TimeAdminDialog</name>
+    <name>DateTime</name>
     <message>
-        <location filename="../timeadmindialog.ui" line="14"/>
-        <source>Time and Date Settings</source>
+        <location filename="../datetime.ui" line="20"/>
+        <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../timeadmindialog.ui" line="23"/>
-        <source>Set current date and time</source>
+        <location filename="../datetime.ui" line="32"/>
+        <source>Time and date setup</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../timeadmindialog.ui" line="30"/>
-        <source>Time zone:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../timeadmindialog.ui" line="40"/>
+        <location filename="../datetime.ui" line="39"/>
         <source>Time:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../timeadmindialog.ui" line="47"/>
-        <source>hh:mm:ss AP</source>
+        <location filename="../datetime.ui" line="55"/>
+        <source>HH:mm:ss</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../timeadmindialog.cpp" line="120"/>
+        <location filename="../datetime.ui" line="69"/>
+        <source>Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="53"/>
+        <source>Date and time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="62"/>
+        <source>Timezone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="162"/>
         <source>Authentication Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>Timezone setup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Your current timezone:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="42"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="51"/>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="70"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
