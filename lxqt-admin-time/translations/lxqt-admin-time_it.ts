b0e9f2ca77457315040f6871edcc7609d1b89548
Stef Binde <stef.binde@riseup.net>
2015-01-25 14:57:57 +0100

    Create lxqt-admin-time_it.ts
    added italian translation file

---
+++ lxqt-admin-time/translations/lxqt-admin-time_it.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT" sourcelanguage="it_IT">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="32"/>
+        <source>Time and date setup</source>
+        <translation>Imposta data e ora</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>Time:</source>
+        <translation>Ora:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="55"/>
+        <source>HH:mm:ss</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="69"/>
+        <source>Date:</source>
+        <translation>Data:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Il salvataggio richiede privilegi di amministratore, 
+saranno chiesti dopo la chiusura di questa finestra.</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Configura data e ora</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="53"/>
+        <source>Date and time</source>
+        <translation>Data e ora</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="62"/>
+        <source>Timezone</source>
+        <translation>Fuso orario</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="162"/>
+        <source>Authentication Error</source>
+        <translation>Errore di autenticazione</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>Timezone setup</source>
+        <translation>Configura fuso orario</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Your current timezone:</source>
+        <translation>Fuso orario attuale:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="42"/>
+        <source>TextLabel</source>
+        <translation>nessuno</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="51"/>
+        <source>Filter</source>
+        <translation>Filtro</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="70"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">Il salvataggio richiede privilegi di amministratore, 
+saranno chiesti dopo la chiusura di questa finestra.</translation>
+    </message>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
