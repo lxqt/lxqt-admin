5db44905d3308044156949ed248d8f40e2fe97cb
zvacet <ikoli@yahoo.com>
2015-05-19 17:06:13 +0200

    Create lxqt-admin-user_hr.ts

---
+++ lxqt-admin-user/translations/lxqt-admin-user_hr.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr_HR">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation type="unfinished">Postavke grupe</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation type="unfinished">Ime grupe:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation type="unfinished">ID grupe</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation type="unfinished">Korisnici koji pripadaju ovoj grupi</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="83"/>
+        <location filename="../groupdialog.cpp" line="92"/>
+        <location filename="../groupdialog.cpp" line="97"/>
+        <source>Error</source>
+        <translation type="unfinished">Greška</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="83"/>
+        <source>The group ID is in use.</source>
+        <translation type="unfinished">ID grupe je u uporabi.</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="92"/>
+        <source>The group name cannot be empty.</source>
+        <translation type="unfinished">Ime grupe ne može biti prazno.</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="97"/>
+        <source>The group name is in use.</source>
+        <translation type="unfinished">Ime grupe je u uporabi.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation type="unfinished">Postavke korisnika i grupe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation type="unfinished">&amp;Korisnici</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation type="unfinished">Ime prijave</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation type="unfinished">ID korisnika</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation type="unfinished">Puno ime</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation type="unfinished">Grupa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation type="unfinished">Osobna mapa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation type="unfinished">&amp;Grupe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation type="unfinished">Ime</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation type="unfinished">ID grupe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation type="unfinished">alatna traka</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation type="unfinished">Dodaj</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation type="unfinished">Dodaj nove korisnike ili grupe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation type="unfinished">Izbriši</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation type="unfinished">Izbriši odabranu stavku</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation type="unfinished">Svojstva</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation type="unfinished">uredi svojstva odabrane stavke</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation type="unfinished">Osvježi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation type="unfinished">Osvježi liste</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation type="unfinished">Greška</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation type="unfinished">Potvrdi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation type="unfinished">Jeste li sigurni da želite izbrisati odabranoga korisnika?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation type="unfinished">Jeste li sigurni da želite izbrisati odabranu grupu?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation type="unfinished">Postavke korisnika</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation type="unfinished">Općenito</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation type="unfinished">Puno ime:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation type="unfinished">Ime prijave:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation type="unfinished">Postavi lozinku:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation type="unfinished">ID korisnika:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation type="unfinished">Glavna grupa:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation type="unfinished">Napredno</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation type="unfinished">Ljuska prijave:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation type="unfinished">Osobna mapa:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="64"/>
+        <source>Change password:</source>
+        <translation type="unfinished">Promjeni lozinku:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="125"/>
+        <location filename="../userdialog.cpp" line="138"/>
+        <location filename="../userdialog.cpp" line="143"/>
+        <source>Error</source>
+        <translation type="unfinished">Greška</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="125"/>
+        <source>The user ID is in use.</source>
+        <translation type="unfinished">ID korisnika je u uporabi.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="138"/>
+        <source>The user name cannot be empty.</source>
+        <translation type="unfinished">Korisničko ime ne može biti prazno.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="143"/>
+        <source>The user name is in use.</source>
+        <translation type="unfinished">Korisničko ime je u uoprabi.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="159"/>
+        <source>Confirm</source>
+        <translation type="unfinished">Potvrdi</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="159"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation type="unfinished">Jeste li sigurni da želite koristiti &quot;praznu lozinku&quot; za korisnika?</translation>
+    </message>
+</context>
+</TS>
