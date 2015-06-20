ac131199b32ae1fd975efe668ebe154e0bf0ba4e
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-20 23:56:25 +0200

    Removed invisible dialog titles. Updated template. Added german translation.

--- lxqt-admin-time/translations/lxqt-admin-time_de.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_de.ts
@@ -1,27 +1,28 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name>DateTime</name>
     <message>
         <location filename="../datetime.ui" line="23"/>
         <source>Time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeit:</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="39"/>
         <source>HH:mm:ss</source>
+        <translatorcomment>No need to translate.</translatorcomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="53"/>
         <source>Date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum:</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="89"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Das Sichern der Einstellungen benötigt Administrator-Rechte.&lt;br&gt;Nach dem Betätigen der Schließen-Schaltfläche wird die Berechtigung eingeholt.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -29,22 +30,22 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="42"/>
         <source>Time and date configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeit und Datum einstellen</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="53"/>
         <source>Date and time</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum und Zeit</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="62"/>
         <source>Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeitzone</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="162"/>
         <source>Authentication Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Authentifizierungsfehler</translation>
     </message>
 </context>
 <context>
@@ -52,27 +53,27 @@
     <message>
         <location filename="../timezone.ui" line="19"/>
         <source>Your current timezone:</source>
-        <translation type="unfinished"></translation>
+        <translation>Derzeitige Zeitzone:</translation>
     </message>
     <message>
         <location filename="../timezone.ui" line="26"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>Textfeld</translation>
     </message>
     <message>
         <location filename="../timezone.ui" line="35"/>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filter</translation>
     </message>
     <message>
         <location filename="../timezone.ui" line="54"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Das Sichern der Einstellungen benötigt Administrator-Rechte.&lt;br&gt;Nach dem Betätigen der Schließen-Schaltfläche wird die Berechtigung eingeholt.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../timezone.cpp" line="42"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine</translation>
     </message>
 </context>
 </TS>
---
+++ lxqt-admin-user/translations/lxqt-admin-user_de.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Gruppeneinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Gruppen-Name:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>Gruppen-ID:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Benutzer in dieser Gruppe:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="86"/>
+        <location filename="../groupdialog.cpp" line="95"/>
+        <location filename="../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>Die Gruppen-ID existiert bereits.</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Der Gruppen-Name kann nicht leer sein.</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>Der Gruppen-Name existiert bereits.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Nutzer- und Gruppeneinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Nutzer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Anmeldename</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>Nutzer-ID</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Vollständiger Name</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Gruppe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Nutzerverzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Systemnutzer anzeigen (nur für erfahrene Nutzer)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Gruppen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>Gruppen-ID</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>Werkzeugleiste</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Neue Nutzer oder Gruppen hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Ausgewähltes Element löschen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Eigenschaften</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>Eigenschaften des ausgewählten Elements editieren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Erneuern</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Listen erneuern</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Bestätigen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Wollen Sie den ausgewählten Nutzer wirklich löschen?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Wollen Sie die ausgewählte Gruppe wirklich löschen?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Nutzereinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Vollständiger Name:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Anmeldename:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Kennwort setzen:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>Nutzer-ID:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Hauptgruppe:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Erweitert</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Befehlsinterpreter (shell):</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Nutzerverzeichnis:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Kennwort ändern:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="128"/>
+        <location filename="../userdialog.cpp" line="141"/>
+        <location filename="../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>Die Nutzer-ID existiert bereits.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Der Nutzername kann nicht leer sein.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>Der Nutzername existiert bereits.</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Bestätigen</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Wollen Sie wirklich ein &quot;leeres Kennwort&quot; für den Nutzer vergeben?</translation>
+    </message>
+</context>
+</TS>
