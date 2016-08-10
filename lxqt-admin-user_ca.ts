90b06059adf139485dae129524ed94ca2c850712
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 22:17:24 +0200

    Add lxqt-admin-user_ca.ts (Catalan)

---
+++ lxqt-admin-user_ca.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Ajusts del grup</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Nom del grup:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>Id. del grup:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Usuaris que pertanyen a aquest grup:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>L&apos;id. del grup ja està en ús.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>El nom del grup no pot estar en blanc.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>El nom del grup ja està en ús.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Ajusts dels usuaris i dels grups</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Usuaris</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Nom d&apos;usuari</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>Id. d&apos;usuari</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Nom complet</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Grup</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Directori d&apos;inici</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Mostra els usuaris del sistema (únicament per als usuaris experimentats)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Grups</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>Id. del grup</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>Barra d&apos;eines</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Afegeix</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Afegeix nous usuaris o grups</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Suprimeix</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Suprimeix l&apos;ítem seleccionat</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Propietats</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>Edita les propietats de l&apos;ítem seleccionat</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Refresca</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Refresca els llistats</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Confirmació</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Esteu segur que voleu suprimir l&apos;usuari seleccionat?</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Esteu segur que voleu suprimir el grup seleccionat?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Ajusts de l&apos;usuari</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Nom complet:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Nom d&apos;usuari:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Establiu la contrasenya:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>Id. d&apos;usuari:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Grup principal:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Avançat</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Shell d&apos;inici de sessió:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Directori d&apos;inici:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Canvieu la contrasenya:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>L&apos;id. de l&apos;usuari ja està en ús.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>El nom de l&apos;usuari no pot estar en blanc.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>El nom de l&apos;usuari ja està en ús.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Confirmació</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Esteu segur que voleu utilitzar una «contrasenya en blanc» per a l&apos;usuari?</translation>
+    </message>
+</context>
+</TS>
