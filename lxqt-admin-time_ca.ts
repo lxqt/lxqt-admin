85da0c47590ab121e600fe7ebc0985a63c706bd2
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 21:48:28 +0200

    Add lxqt-admin-time_ca.ts (Catalan)

---
+++ lxqt-admin-time_ca.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Hora:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Data:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Configuració de la data i l&apos;hora</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="53"/>
+        <source>Date and time</source>
+        <translation>Data i hora</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="62"/>
+        <source>Timezone</source>
+        <translation>Zona horària</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="162"/>
+        <source>Authentication Error</source>
+        <translation>Error d&apos;autenticació</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>La vostra zona horària actual:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>Etiqueta de text</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Filtre</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="54"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Sense</translation>
+    </message>
+</context>
+</TS>
