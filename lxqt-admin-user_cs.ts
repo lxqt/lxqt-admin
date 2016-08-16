a97854b0f31ecddc9591c49833d03d7c85ff90cd
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lxqt-admin-user_cs.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Nastavení skupiny</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Název skupiny:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>Identifikátor skupiny:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Uživatelé patřící do této skupiny:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>Identifikátor skupiny se již používá.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Název skupiny nemůže být prázdný.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>Název skupiny se již používá.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Nastavení uživatele a skupiny</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Uživatelé</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Název přihlášení</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>Identifikátor uživatele</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Celé jméno</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Skupina</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Domovský adresář</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Ukázat systémové uživatele (pouze pro pokročilé uživatele)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Skupiny</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Název</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>Identifikátor skupiny</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>Nástrojový pruh</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Přidat</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Přidat nové uživatele a skupiny</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Smazat</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Smazat vybranou položku</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Vlastnosti</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>Upravit vlastnosti vybrané položky</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Obnovit</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Obnovit seznam</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Potvrdit</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Jste si jistý, že chcete smazat vybraného uživatele?</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Jste si jistý, že chcete smazat vybranou skupinu?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Nastavení uživatele</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Obecné</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Celé jméno:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Název přihlášení:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Nastavit heslo:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>Identifikátor uživatele:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Hlavní skupina:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Pokročilé</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Příkazový interpret (shell):</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Domovský adresář:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Změnit heslo:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>Identifikátor uživatele se již používá.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Jméno uživatele nemůže být prázdný.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>Jméno uživatele se již používá.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Potvrdit</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Jste si jistý, že chcete pro uživatele použít &quot;prázdné heslo&quot;?</translation>
+    </message>
+</context>
+</TS>
