9c20f28eb092c21448ae6278099b6b9adecd8f4f
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

---
+++ lxqt-admin-time_uk.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Час:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>Год:хв:сек</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Дата:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Збереження змін вимагає прав суперкористувача.&lt;br&gt;Про них Вас запитають після натиснення кнопки закривання&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Налаштування часу і дати</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="53"/>
+        <source>Date and time</source>
+        <translation>Дата і час</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="62"/>
+        <source>Timezone</source>
+        <translation>Часова зона</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="162"/>
+        <source>Authentication Error</source>
+        <translation>Помилка розпізнавання</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Ваша теперішня часова зона:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>ТекстоваМітка</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Фільтр</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="54"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Зберігання змін вимагає прав суперкористувача.&lt;br&gt;Вас про них запитають після натискання кнопки закривання&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Нічого</translation>
+    </message>
+</context>
+</TS>
