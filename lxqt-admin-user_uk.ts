439c7dbcdeee9b7d3516429a42f1199f83bf1680
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

---
+++ lxqt-admin-user_uk.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Налаштування груп</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Назва групи:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>ID групи:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Користувачі, що належать до цієї групи:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>Ця ID групи вже використовується.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Назва групи не може бути порожньою.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>Ця назва групи вже використовується.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Налаштування користувачів і груп</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Користувачі</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Назва користувача</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>ID користувача</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Повна назва</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Група</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Домашня тека</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Показати системних користувачів (тільки для досвідчених користувачів)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Групи</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Назва</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>ID групи</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>панельІнструментів</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Додати</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Додати нових користувачів або групи</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Вилучити</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Вилучити вибрані</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Властивості</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>редагувати властивості вибраних пунктів</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Оновити</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Оновити список</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Підтвердити</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Ви дійсно бажаєте вилучити наступного користувача?</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Ви дійсно бажаєте вилучити наступну групу?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Налаштування користувачів</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Загальне</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Повна назва:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Назва користувача:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Встановити пароль:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>ID користувача:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Основна група:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Більше</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Shell логування:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Домашня тека:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Зміна паролю:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>Ця ID користувача вже використовується.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Назва користувача не може бути порожньою.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>Ця назва користувача вже використовується.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Підтвердити</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Ви дійсно бажаєте використати &quot;порожній пароль&quot; для користувача?</translation>
+    </message>
+</context>
+</TS>
