673f730330b5a18f64101750bdd113440a8bb63c
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

---
+++ lxqt-admin-user_tr.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Grup Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Grup adı:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>Grup ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Bu gruba ait kullanıcılar:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>Grup ID kullanılıyor.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Grup adı boş bırakılamaz.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>Grup adı kullanılıyor.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Kullanıcı ve Grup Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Kullanıcılar</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Giriş Adı</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>Kullanıcı ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Tam Adı</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Grup</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Ev Dizini</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Sistem Kullanıcılarını göster (sadece ileri düzey kullanıcılar için)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Grup</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Adı</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>Gup ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>Araç Çubuğu</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Ekle</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Yeni kullanıcılar ve gruplar ekle</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Sil</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Seçilen ögeyi sil</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Özellikler</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>seçilen ögenin özelliklerini düzenle</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Listeyi yenile</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Doğrula</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Seçilen kullanıcıyı silmek istediğinizden emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Seçilen grubu silmek istediğinizden emin misiniz?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Kullanıcı Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Genel</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Tam adı:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Giriş adı:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Parola:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>Kullanıcı ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Ana grup:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Gelişmiş</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Giriş kabuğu:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Ev dizini:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Parolayı değiştir:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>Kullanıcı ID kullanılıyor.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Kullanıcı adı boş bırakılamaz.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>Kullanıcı adı kullanılıyor.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Doğrula</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Kullanıcı için &quot;boş parola&quot; kullanmak istediğinizden emin misiniz?</translation>
+    </message>
+</context>
+</TS>
