6506ae7239c8fc728b97abf3d9689166aba1aefc
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

---
+++ lxqt-admin-user_lt.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Grupės nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Grupės pavadinimas:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="37"/>
+        <source>Group ID:</source>
+        <translation>Grupės ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="44"/>
+        <source>Users belong to this group:</source>
+        <translation>Šiai grupei priklausantys naudotojai:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>Error</source>
+        <translation>Klaida</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="86"/>
+        <source>The group ID is in use.</source>
+        <translation>Grupės ID yra naudojamas.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="95"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Grupės pavadinimas negali būti tuščias.</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="100"/>
+        <source>The group name is in use.</source>
+        <translation>Grupės pavadinimas yra naudojamas.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Naudotojų ir grupių nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="31"/>
+        <source>&amp;Users</source>
+        <translation>Na&amp;udotojai</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="50"/>
+        <source>Login Name</source>
+        <translation>Prisijungimo vardas</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="55"/>
+        <source>User ID</source>
+        <translation>Naudotojo ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="60"/>
+        <source>Full Name</source>
+        <translation>Visas vardas</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="65"/>
+        <source>Group</source>
+        <translation>Grupė</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="70"/>
+        <source>Home Directory</source>
+        <translation>Namų katalogas</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="81"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Rodyti sistemos naudotojus (tik pažengusiems naudotojams)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="89"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Grupės</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="102"/>
+        <source>Name</source>
+        <translation>Pavadinimas</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="107"/>
+        <source>Group ID</source>
+        <translation>Grupės ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="120"/>
+        <source>toolBar</source>
+        <translation>Įrankių juosta</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="147"/>
+        <source>Add</source>
+        <translation>Pridėti</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="150"/>
+        <source>Add new users or groups</source>
+        <translation>Pridėti naujus naudotojus ar grupes</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="160"/>
+        <source>Delete</source>
+        <translation>Ištrinti</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="163"/>
+        <source>Delete selected item</source>
+        <translation>Ištrinti pasirinktą elementą</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Properties</source>
+        <translation>Savybės</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="176"/>
+        <source>edit properties of the selected item</source>
+        <translation>Keisti pasirinkto elemento savybes</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="186"/>
+        <source>Refresh</source>
+        <translation>Įkelti iš naujo</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="189"/>
+        <source>Refresh the lists</source>
+        <translation>Įkelti sąrašus iš naujo</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="145"/>
+        <source>Error</source>
+        <translation>Klaida</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Confirm</source>
+        <translation>Patvirtinti</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Ar tikrai norite ištrinti pasirinktą naudotoją?</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="204"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Ar tikrai norite ištrinti pasirinktą grupę?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Naudotojo nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Bendra</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Full name:</source>
+        <translation>Visas vardas:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Login name:</source>
+        <translation>Prisijungimo vardas:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>Set password:</source>
+        <translation>Nustatyti slaptažodį:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="77"/>
+        <source>User ID:</source>
+        <translation>Naudotojo ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="84"/>
+        <source>Main group:</source>
+        <translation>Pagrindinė grupė:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="99"/>
+        <source>Advanced</source>
+        <translation>Išplėstiniai</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="105"/>
+        <source>Login shell:</source>
+        <translation>Prisijungimo apvalkalas:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="122"/>
+        <source>Home directory:</source>
+        <translation>Namų katalogas:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="67"/>
+        <source>Change password:</source>
+        <translation>Keisti slaptažodį:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>Error</source>
+        <translation>Klaida</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="128"/>
+        <source>The user ID is in use.</source>
+        <translation>Naudotojo ID yra naudojamas.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="141"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Naudotojo vardas negali būti tuščias.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="146"/>
+        <source>The user name is in use.</source>
+        <translation>Naudotojo vardas yra naudojamas.</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Confirm</source>
+        <translation>Patvirtinti</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="162"/>
+        <source>Are you sure you want to use an &quot;empty password&quot; for the user?</source>
+        <translation>Ar tikrai naudotojui norite naudoti &quot;tuščią slaptažodį&quot;?</translation>
+    </message>
+</context>
+</TS>
