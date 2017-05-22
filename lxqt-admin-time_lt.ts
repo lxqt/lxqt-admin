ca5cbda53e522dcc32c693a337c30cd2f9a90b40
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

---
+++ lxqt-admin-time_lt.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Laikas:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Data:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Pakeitimų įrašymas reikalauja administratoriaus teisių.&lt;br&gt;Jūsų bus užklausta po to, kai spustelėsite užvėrimo mygtuką&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Laiko ir datos konfigūravimas</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="53"/>
+        <source>Date and time</source>
+        <translation>Data ir laikas</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="62"/>
+        <source>Timezone</source>
+        <translation>Laiko juosta</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="162"/>
+        <source>Authentication Error</source>
+        <translation>Tapatybės nustatymo klaida</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Jūsų esama laiko juosta:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>Teksto etiketė</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Filtruoti</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="54"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Pakeitimų įrašymas reikalauja administratoriaus teisių.&lt;br&gt;Jūsų bus užklausta po to, kai spustelėsite užvėrimo mygtuką&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Nėra</translation>
+    </message>
+</context>
+</TS>
