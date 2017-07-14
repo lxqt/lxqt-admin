a94ce95e18543ffc8df25035740b2a1c55faef86
Marcin Mikołajczak <me@m4sk.in>
2017-07-14 10:38:16 +0200

    Updated Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

--- lxqt-admin-time_pl.ts
+++ lxqt-admin-time_pl.ts
@@ -25,12 +25,12 @@
     <message>
         <location filename="../../../datetime.ui" line="70"/>
         <source>Enable network time synchronization (NTP)</source>
-        <translation type="unfinished"></translation>
+        <translation>Włącz sieciową synchronizację czasu (NTP)</translation>
     </message>
     <message>
         <location filename="../../../datetime.ui" line="77"/>
         <source>RTC is in local time</source>
-        <translation type="unfinished"></translation>
+        <translation>RTC jest w czasie lokalnym</translation>
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
@@ -60,7 +60,7 @@
         <location filename="../../../timeadmindialog.cpp" line="134"/>
         <location filename="../../../timeadmindialog.cpp" line="144"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
     <message>
         <source>Authentication Error</source>
@@ -102,7 +102,7 @@
     <message>
         <location filename="../../../timezone.cpp" line="42"/>
         <source>None</source>
-        <translation type="unfinished">Żadna</translation>
+        <translation>Żadna</translation>
     </message>
 </context>
 </TS>
