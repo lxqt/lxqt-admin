c5d5b22b7763d3ad4bab45bfa756c645f0d83568
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-07-14 13:12:55 +0200

    Updated Catalan translation

--- lxqt-admin-time_ca.ts
+++ lxqt-admin-time_ca.ts
@@ -21,16 +21,16 @@
     <message>
         <location filename="../../../datetime.ui" line="70"/>
         <source>Enable network time synchronization (NTP)</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilita la sincronització de l'hora a través de NTP (Network Time Protocol)</translation>
     </message>
     <message>
         <location filename="../../../datetime.ui" line="77"/>
         <source>RTC is in local time</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;RTC (Real-Time Clock) té l&apos;hora local</translation>
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -56,11 +56,11 @@
         <location filename="../../../timeadmindialog.cpp" line="134"/>
         <location filename="../../../timeadmindialog.cpp" line="144"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error</translation>
     </message>
     <message>
         <source>Authentication Error</source>
-        <translation type="vanished">Error d&apos;autenticació</translation>
+        <translation>Error d&apos;autenticació</translation>
     </message>
 </context>
 <context>
@@ -82,11 +82,11 @@
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;El desament dels canvis requereix permisos d&apos;administració.&lt;br&gt;Se us demanaran després que feu clic al botó tanca&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="vanished">Sense</translation>
+        <translation>Sense</translation>
     </message>
 </context>
 <context>
@@ -94,7 +94,7 @@
     <message>
         <location filename="../../../timezone.cpp" line="42"/>
         <source>None</source>
-        <translation type="unfinished">Sense</translation>
+        <translation>Sense</translation>
     </message>
 </context>
 </TS>
