9cd2123b8849fc22faba8cbbc60d380c87cd7b48
Toms Tilgalis <ttilgalis@gmail.com>
2017-09-06 00:33:49 +0300

    Latvian - Admin time, Cursor, Brightness, File Assoc., Config

---
+++ lxqt-admin-time_lv.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../../../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Laiks:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Datums:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Iespējot tīkla laika sinhronizāciju NTP (Network Time Protocol)</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC (Real-Time Clock) ir lokālajā laikā</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Izmaiņu saglabāšanai vajadzīgas administrēšanas atļaujas.&lt;br&gt;Jūs tiksiet pieprasīts pēc Aizvērt pogas nospiešanas&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Laika un datuma konfigurācija</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="48"/>
+        <source>Date and time</source>
+        <translation>Datums un laiks</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="56"/>
+        <source>Timezone</source>
+        <translation>Laika zona</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="117"/>
+        <location filename="../../../timeadmindialog.cpp" line="127"/>
+        <location filename="../../../timeadmindialog.cpp" line="134"/>
+        <location filename="../../../timeadmindialog.cpp" line="144"/>
+        <source>Error</source>
+        <translation>Kļūda</translation>
+    </message>
+    <message>
+        <source>Authentication Error</source>
+        <translation type="vanished">Autorizācijas Kļūda</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../../../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Pašreizējā laikazona:</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>Teksta etiķete</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Filtrs</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Saving changes requires admin permissions.&lt;br&gt;You will be requested after clicking close button&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Izmaiņu saglabāšanai vajadzīgas administrēšanas atļaujas.&lt;br&gt;Jūs tiksiet pieprasīts pēc Aizvērt pogas nospiešanas&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="vanished">Neviens/-a</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../../../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Neviena</translation>
+    </message>
+</context>
+</TS>
