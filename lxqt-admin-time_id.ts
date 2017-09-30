7f21fa35e76ece0a9ff62b0eea531907efedd55c
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

---
+++ lxqt-admin-time_id.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../../../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Waktu:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>JJ:mm:dd</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Tanggal:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Aktifkan network time synchronization (NTP)</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC adalah waktu lokal</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Konfigurasi waktu dan tanggal</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="48"/>
+        <source>Date and time</source>
+        <translation>Tanggal dan waktu</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="56"/>
+        <source>Timezone</source>
+        <translation>Zona waktu</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="117"/>
+        <location filename="../../../timeadmindialog.cpp" line="127"/>
+        <location filename="../../../timeadmindialog.cpp" line="134"/>
+        <location filename="../../../timeadmindialog.cpp" line="144"/>
+        <source>Error</source>
+        <translation>Kesalahan</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../../../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Zona waktu Anda saat ini:</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../../../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Tidak ada</translation>
+    </message>
+</context>
+</TS>
