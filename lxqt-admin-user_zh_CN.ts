8c8cda7304ec36ce1c48aedcdfd0f695877a923d
notname000 <notname000@outlook.com>
2018-01-29 18:53:59 +0800

    Update zh_CN translations

---
+++ lxqt-admin-user_zh_CN.ts
@@ -0,0 +1,273 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../../../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>组设置</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>组名称:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="30"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="40"/>
+        <source>Group ID:</source>
+        <translation>组 ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.ui" line="47"/>
+        <source>Users belong to this group:</source>
+        <translation>组成员:</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="64"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../../../groupdialog.cpp" line="64"/>
+        <source>The group name cannot be empty.</source>
+        <translation>组名称不得为空。</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>用户与组设置</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="40"/>
+        <source>&amp;Users</source>
+        <translation>用户(&amp;U)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="56"/>
+        <source>Login Name</source>
+        <translation>登录名</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="61"/>
+        <source>User ID</source>
+        <translation>用户 ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="66"/>
+        <source>Full Name</source>
+        <translation>全名</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="71"/>
+        <source>Group</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="76"/>
+        <source>Home Directory</source>
+        <translation>主目录</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="87"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>显示系统用户（高级）</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="95"/>
+        <source>&amp;Groups</source>
+        <translation>组(&amp;G)</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="108"/>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="113"/>
+        <source>Group ID</source>
+        <translation>组 ID</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="118"/>
+        <source>Members</source>
+        <translation>成员</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="131"/>
+        <source>toolBar</source>
+        <translation>工具栏</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="158"/>
+        <source>Add</source>
+        <translation>新增</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="161"/>
+        <source>Add new users or groups</source>
+        <translation>新增用户或组</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="170"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="173"/>
+        <source>Delete selected item</source>
+        <translation>删除所选项</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="182"/>
+        <source>Properties</source>
+        <translation>属性</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="185"/>
+        <source>edit properties of the selected item</source>
+        <translation>编辑所选项的属性</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="194"/>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="197"/>
+        <source>Refresh the lists</source>
+        <translation>刷新列表</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="206"/>
+        <source>Change Password</source>
+        <translation>更改密码</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.ui" line="209"/>
+        <source>Change password for the selected user or group</source>
+        <translation>更改所选用户或组的密码</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="164"/>
+        <location filename="../../../mainwindow.cpp" line="176"/>
+        <location filename="../../../mainwindow.cpp" line="197"/>
+        <source>Confirm</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="164"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>是否确认删除所选的用户？</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="176"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>是否确认删除所选的组？</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="187"/>
+        <source>Input the new password for %1:</source>
+        <translation>输入 %1 的新密码:</translation>
+    </message>
+    <message>
+        <location filename="../../../mainwindow.cpp" line="197"/>
+        <source>Are you sure you want to set a empty password?</source>
+        <translation>是否确认设置空密码？</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../../../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>用户设置</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="43"/>
+        <source>Full name:</source>
+        <translation>全名:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="33"/>
+        <source>Login name:</source>
+        <translation>登录名:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="53"/>
+        <source>User ID:</source>
+        <translation>用户 ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="60"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="70"/>
+        <source>Main group:</source>
+        <translation>主要组:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="85"/>
+        <source>Groups</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="91"/>
+        <source>The user belongs to the following groups:</source>
+        <translation>该用户隶属于下列组:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="109"/>
+        <source>Advanced</source>
+        <translation>高级</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="115"/>
+        <source>Login shell:</source>
+        <translation>登录 Shell:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.ui" line="132"/>
+        <source>Home directory:</source>
+        <translation>主目录:</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="117"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../../../userdialog.cpp" line="117"/>
+        <source>The user name cannot be empty.</source>
+        <translation>用户名不得为空。</translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../../../usermanager.cpp" line="224"/>
+        <source>lxqt-admin-user</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../usermanager.cpp" line="225"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;动作 (%1) 失败：&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+</TS>
