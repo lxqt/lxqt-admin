d71280bbb150b06c38c6e49ccb977ba7cbbc0d80
notname000 <notname000@outlook.com>
2018-01-29 18:53:59 +0800

    Update zh_CN translations

---
+++ lxqt-admin-time_zh_CN.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../../../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>时间:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>日期:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>开启网络时间同步（NTP）</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC 在本地时间</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>时间与日期配置</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="48"/>
+        <source>Date and time</source>
+        <translation>日期与时间</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="56"/>
+        <source>Timezone</source>
+        <translation>时区</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="117"/>
+        <location filename="../../../timeadmindialog.cpp" line="127"/>
+        <location filename="../../../timeadmindialog.cpp" line="134"/>
+        <location filename="../../../timeadmindialog.cpp" line="144"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../../../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>当前时区:</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation>文本标签</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>过滤器</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../../../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+</context>
+</TS>
