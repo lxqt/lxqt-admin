87734670e7ce1c5bd49744beab663e0733694f13
yo <yo@ejemplo.com>
2018-04-28 16:01:42 +0200

    Complete Spanish translation

---
+++ lxqt-admin-time_es.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="es">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../../../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Hora:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Fecha:</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Habilitar la sincronización de la hora desde la red (NTP)</translation>
+    </message>
+    <message>
+        <location filename="../../../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC está en horario local</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="42"/>
+        <source>Time and date configuration</source>
+        <translation>Configuración de hora y fecha</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="48"/>
+        <source>Date and time</source>
+        <translation>Fecha y hora</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="56"/>
+        <source>Timezone</source>
+        <translation>Zona horaria</translation>
+    </message>
+    <message>
+        <location filename="../../../timeadmindialog.cpp" line="117"/>
+        <location filename="../../../timeadmindialog.cpp" line="127"/>
+        <location filename="../../../timeadmindialog.cpp" line="134"/>
+        <location filename="../../../timeadmindialog.cpp" line="144"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../../../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Su zona horaria actual:</translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation>Filtro</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../../../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Ninguno</translation>
+    </message>
+</context>
+</TS>
