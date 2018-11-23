d7371495dd1149eeef7697f3f6a1313684d526a9
macarini2018 <andrey.macarini@gmail.com>
2018-11-23 18:12:41 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ lxqt-admin-time/translations/lxqt-admin-time_pt_BR.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="45"/>
+        <source>Time and date configuration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="51"/>
+        <source>Date and time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="59"/>
+        <source>Timezone</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="120"/>
+        <location filename="../timeadmindialog.cpp" line="130"/>
+        <location filename="../timeadmindialog.cpp" line="137"/>
+        <location filename="../timeadmindialog.cpp" line="147"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="35"/>
+        <source>Filter</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
