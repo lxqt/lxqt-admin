88ed6afe708ba72d4be97e59f56a95913e1e0493
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2018-12-13 05:34:46 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100,0% (51 of 51 strings)
    
    Translation: LXQt/LXQt Admin (User)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-user/tr/

--- lxqt-admin-user/translations/lxqt-admin-user_tr.ts
+++ lxqt-admin-user/translations/lxqt-admin-user_tr.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../groupdialog.ui" line="30"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Varsayılan</translation>
     </message>
     <message>
         <location filename="../groupdialog.ui" line="40"/>
@@ -99,12 +99,12 @@
     <message>
         <location filename="../mainwindow.ui" line="121"/>
         <source>Members</source>
-        <translation type="unfinished"></translation>
+        <translation>Üyeler</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="134"/>
         <source>toolBar</source>
-        <translation>Araç Çubuğu</translation>
+        <translation>Araç çubuğu</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="161"/>
@@ -149,12 +149,12 @@
     <message>
         <location filename="../mainwindow.ui" line="209"/>
         <source>Change Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Parolayı Değiştir</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="212"/>
         <source>Change password for the selected user or group</source>
-        <translation type="unfinished"></translation>
+        <translation>Seçilen kullanıcı ya da grup için parolayı değiştir</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="177"/>
@@ -176,12 +176,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="200"/>
         <source>Input the new password for %1:</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 için yeni parolayı girin:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="210"/>
         <source>Are you sure you want to set a empty password?</source>
-        <translation type="unfinished"></translation>
+        <translation>Parolayı boş bırakmak istediğinizden emin misiniz?</translation>
     </message>
 </context>
 <context>
@@ -214,7 +214,7 @@
     <message>
         <location filename="../userdialog.ui" line="60"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Varsayılan</translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="70"/>
@@ -224,12 +224,12 @@
     <message>
         <location filename="../userdialog.ui" line="85"/>
         <source>Groups</source>
-        <translation type="unfinished"></translation>
+        <translation>Gruplar</translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="91"/>
         <source>The user belongs to the following groups:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı aşağıdaki gruplara ait:</translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="109"/>
@@ -262,12 +262,12 @@
     <message>
         <location filename="../usermanager.cpp" line="227"/>
         <source>lxqt-admin-user</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-yönetici-kullanıcı</translation>
     </message>
     <message>
         <location filename="../usermanager.cpp" line="228"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Eylem (%1) başarısız:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 </TS>
