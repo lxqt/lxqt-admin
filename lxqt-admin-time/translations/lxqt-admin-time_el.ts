7d716420d8d47d995aad577b4273d001658e74a2
Dimitrios Glentadakis <dglent@free.fr>
2018-12-16 05:48:50 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/el/

--- lxqt-admin-time/translations/lxqt-admin-time_el.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_el.ts
@@ -34,12 +34,12 @@
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-admin-time-helper</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Αποτυχία της ενέργειας (%1):&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -70,17 +70,17 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in localtime requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Η αλλαγή του ρολογιού πραγματικού χρόνου (RTC) στην τοπική ώρα απαιτεί επανεκκίνηση</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in UTC requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Η αλλαγή του ρολογιού πραγματικού χρόνου (RTC) στην συντονισμένη παγκόσμια ώρα (UTC) απαιτεί επανεκκίνηση</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="155"/>
         <source>Reboot required</source>
-        <translation type="unfinished"></translation>
+        <translation>Απαιτείται επανεκκίνηση</translation>
     </message>
 </context>
 <context>
