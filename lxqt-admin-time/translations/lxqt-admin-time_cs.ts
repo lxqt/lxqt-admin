0db479ee6031a0a7aff60540b0a9ac34aac98af4
PeterSzakacs <petersz256@gmail.com>
2018-12-26 16:19:56 +0000

    Translated using Weblate (Czech)
    Currently translated at 94,4% (17 of 18 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/cs/

--- lxqt-admin-time/translations/lxqt-admin-time_cs.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_cs.ts
@@ -80,7 +80,7 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="155"/>
         <source>Reboot required</source>
-        <translation>Vyžadován restart</translation>
+        <translation>Vyžaduje se restart počítače</translation>
     </message>
 </context>
 <context>
