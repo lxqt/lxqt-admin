390b23b510d8d9ab0bfe567e7a3eb53c6a49d324
Moo <hazap@hotmail.com>
2019-01-21 12:19:27 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: LXQt/LXQt Admin (User)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-user/lt/

--- lxqt-admin-user/translations/lxqt-admin-user_lt.ts
+++ lxqt-admin-user/translations/lxqt-admin-user_lt.ts
@@ -104,7 +104,7 @@
     <message>
         <location filename="../mainwindow.ui" line="134"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Įrankių juosta</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="161"/>
@@ -134,7 +134,7 @@
     <message>
         <location filename="../mainwindow.ui" line="188"/>
         <source>edit properties of the selected item</source>
-        <translation type="unfinished"></translation>
+        <translation>taisyti pasirinkto elemento savybes</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="197"/>
