f1afc9cf2af57e384206c89903479526b8670ebf
Marcin Mikołajczak <marcin123407@vivaldi.net>
2019-01-27 21:46:21 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (50 of 50 strings)
    
    Translation: LXQt/LXQt Admin (User)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-user/pl/

--- lxqt-admin-user/translations/lxqt-admin-user_pl.ts
+++ lxqt-admin-user/translations/lxqt-admin-user_pl.ts
@@ -104,7 +104,7 @@
     <message>
         <location filename="../mainwindow.ui" line="185"/>
         <source>Edit properties of the selected item</source>
-        <translation type="unfinished"></translation>
+        <translation>Edytuj właściwości zaznaczonego elementu</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="158"/>
