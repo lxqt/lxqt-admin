599f0faa717d71b1cce76aaac5545df2ac40ff80
Marcin Mikołajczak <marcin123407@vivaldi.net>
2019-01-27 21:57:42 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/pl/

--- lxqt-admin-time/translations/lxqt-admin-time_pl.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_pl.ts
@@ -34,12 +34,12 @@
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-admin-time-helper</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Działanie (%1) nie powiodło się:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -70,17 +70,17 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in localtime requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmiana RTC na czas lokalny wymaga ponownego uruchomienia</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in UTC requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmiana RTC na UTC wymaga ponownego uruchomienia</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="155"/>
         <source>Reboot required</source>
-        <translation type="unfinished"></translation>
+        <translation>Wymagane jest ponowne uruchomienie</translation>
     </message>
 </context>
 <context>
