b8d53afb2d86e0a9fcc5b5b29d5ade1d1c247f4a
roxfr <roxfr@outlook.fr>
2019-02-19 21:27:00 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/fr/

--- lxqt-admin-time/translations/lxqt-admin-time_fr.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_fr.ts
@@ -34,12 +34,12 @@
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-admin-aide-temps</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Action (%1) échouée:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -70,17 +70,17 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in localtime requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer RTC pour prendre en compte le temps local requiert un redémarrage</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in UTC requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer RTC pour être en UTC requiert un redémarrage</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="155"/>
         <source>Reboot required</source>
-        <translation type="unfinished"></translation>
+        <translation>Redémarrage requis</translation>
     </message>
 </context>
 <context>
