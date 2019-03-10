ba7c32bbad5ac8ee40c67ced3776012044429df6
Jan Jasper de Kroon <jajadekroon@gmail.com>
2019-03-10 16:51:30 +0000

    Translated using Weblate (Dutch)
    Currently translated at 76.5% (13 of 17 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/nl/

--- lxqt-admin-time/translations/lxqt-admin-time_nl.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_nl.ts
@@ -35,7 +35,7 @@
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-admin-time-helper</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
