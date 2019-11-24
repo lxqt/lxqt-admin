2ab023f4dc15fbec6df51c848cc1d0f99c627f82
Kim Tinh <onlyesterday.16@gmail.com>
2019-11-24 17:27:38 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Admin (Time)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-admin-time/vi/

--- lxqt-admin-time/translations/lxqt-admin-time_vi.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_vi.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../datetime.ui" line="23"/>
         <source>Time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Giờ:</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="39"/>
         <source>HH:mm:ss</source>
-        <translation type="unfinished"></translation>
+        <translation>Giờ:phút:giây</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="53"/>
         <source>Date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngày:</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="70"/>
         <source>Enable network time synchronization (NTP)</source>
-        <translation type="unfinished"></translation>
+        <translation>Bật đồng bộ thời gian qua mạng (NTP)</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="77"/>
         <source>RTC is in local time</source>
-        <translation type="unfinished"></translation>
+        <translation>RTC trong giờ cục bộ</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>Trợ giúp quản trị LXQt (Thời gian)</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Hành động (%1) thất bại:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -47,17 +47,17 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="45"/>
         <source>Time and date configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Cầu hình ngày giờ</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="51"/>
         <source>Date and time</source>
-        <translation type="unfinished"></translation>
+        <translation>Ngày và giờ</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="59"/>
         <source>Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Múi giờ</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="120"/>
@@ -65,22 +65,22 @@
         <location filename="../timeadmindialog.cpp" line="137"/>
         <location filename="../timeadmindialog.cpp" line="147"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Lỗi</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in localtime requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Đổi RTC thành giờ cục bộ yêu cầu khởi động lại</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="154"/>
         <source>Change RTC to be in UTC requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Đổi RTC thành UTC yêu cầu khởi động lại</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="155"/>
         <source>Reboot required</source>
-        <translation type="unfinished"></translation>
+        <translation>Yêu cầu khởi động lại</translation>
     </message>
 </context>
 <context>
@@ -88,12 +88,12 @@
     <message>
         <location filename="../timezone.ui" line="19"/>
         <source>Your current timezone:</source>
-        <translation type="unfinished"></translation>
+        <translation>Múi giờ hiện tại:</translation>
     </message>
     <message>
         <location filename="../timezone.ui" line="32"/>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ lọc</translation>
     </message>
 </context>
 <context>
@@ -101,7 +101,7 @@
     <message>
         <location filename="../timezone.cpp" line="42"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Không</translation>
     </message>
 </context>
 </TS>
