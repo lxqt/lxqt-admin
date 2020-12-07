ea2fdac0b1e2167cd738fbcdabc149e1487804ca
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-12-07 19:05:51 +0100

    weblate commits (#220)
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/bg/
    Translation: LXQt Configuration/lxqt-admin-time
    Translation: LXQt Configuration/lxqt-admin-time Menu Entry
    Translation: LXQt Configuration/lxqt-admin-user
    Translation: LXQt Configuration/lxqt-admin-user Menu Entry
    
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>

---
+++ lxqt-admin-time/translations/lxqt-admin-time_bg.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Час:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Дата:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Активиране на синхронизирането на времето от интернет(NTP)</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>Системният часовник (RTC) е в локално време</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="122"/>
+        <source>lxqt-admin-time-helper</source>
+        <translation>lxqt-admin-time-helper</translation>
+    </message>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="123"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Действие (%1) неуспешно:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="45"/>
+        <source>Time and date configuration</source>
+        <translation>Настройки на час и дата</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="51"/>
+        <source>Date and time</source>
+        <translation>Час и дата</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="59"/>
+        <source>Timezone</source>
+        <translation>Часова зона</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="120"/>
+        <location filename="../timeadmindialog.cpp" line="130"/>
+        <location filename="../timeadmindialog.cpp" line="137"/>
+        <location filename="../timeadmindialog.cpp" line="147"/>
+        <source>Error</source>
+        <translation>Грешка</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="154"/>
+        <source>Change RTC to be in localtime requires a reboot</source>
+        <translation>Промяната на RTC, за да бъде в локално време, изисква рестартиране</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="154"/>
+        <source>Change RTC to be in UTC requires a reboot</source>
+        <translation>Промяната на RTC, за да бъде в UTC, изисква рестартиране</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="155"/>
+        <source>Reboot required</source>
+        <translation>Изисква се рестартиране</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Вашата актуална часова зона:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="32"/>
+        <source>Filter</source>
+        <translation>Филтър</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Без</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-admin-user/translations/lxqt-admin-user_bg.ts
@@ -0,0 +1,268 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Настройки на група</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Име на група:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="30"/>
+        <source>Default</source>
+        <translation>По подразбиране</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="40"/>
+        <source>Group ID:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="47"/>
+        <source>Users belong to this group:</source>
+        <translation>Потребители в групата:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>Error</source>
+        <translation>Грешка</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Името на групата не може да бъде празно.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Настройки на потребител и група</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="40"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Потребители</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Login Name</source>
+        <translation>Потребителско име</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>User ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Full Name</source>
+        <translation>Пълно име</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Group</source>
+        <translation>Група</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="79"/>
+        <source>Home Directory</source>
+        <translation>Домашна директория</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Показване на системните потребител (само за напреднали)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="95"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Групи</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Group ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Members</source>
+        <translation>Членове</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <source>Edit properties of the selected item</source>
+        <translation>Редактиране на свойствата на избрания елемент</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>Add</source>
+        <translation>Добавяне</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>Add new users or groups</source>
+        <translation>Добавяне на нов потребител и група</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="170"/>
+        <source>Delete</source>
+        <translation>Изтриване</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Delete selected item</source>
+        <translation>Изтриване на избрания елемент</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="182"/>
+        <source>Properties</source>
+        <translation>Свойства</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="194"/>
+        <source>Refresh</source>
+        <translation>Опресняване</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="197"/>
+        <source>Refresh the lists</source>
+        <translation>Опресняване на списъка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="206"/>
+        <source>Change Password</source>
+        <translation>Смяна на парола</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="209"/>
+        <source>Change password for the selected user or group</source>
+        <translation>Смяна на парола на избрания потребител и група</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Confirm</source>
+        <translation>Потвърждаване</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Сигурни ли сте, че искате да изтриете избрания потребител?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Сигурни ли сте, че искате да изтриете избраната група?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="200"/>
+        <source>Input the new password for %1:</source>
+        <translation>Въведете нова парола за %1:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Are you sure you want to set a empty password?</source>
+        <translation>Сигурни ли сте, че искате да зададете празна парола?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Настройки на потребител</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Общи</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Full name:</source>
+        <translation>Пълно име:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Login name:</source>
+        <translation>Потребителско име:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>User ID:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="60"/>
+        <source>Default</source>
+        <translation>По подразбиране</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="70"/>
+        <source>Main group:</source>
+        <translation>Основна група:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="85"/>
+        <source>Groups</source>
+        <translation>Групи</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="91"/>
+        <source>The user belongs to the following groups:</source>
+        <translation>Потребителят принадлежи към следните групи:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="109"/>
+        <source>Advanced</source>
+        <translation>Разширени</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="115"/>
+        <source>Login shell:</source>
+        <translation>Команден интерпретатор:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="132"/>
+        <source>Home directory:</source>
+        <translation>Домашна директория:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>Error</source>
+        <translation>Грешка</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Потребителското име не може да бъде празно.</translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../usermanager.cpp" line="227"/>
+        <source>lxqt-admin-user</source>
+        <translation>Смяна на паролата</translation>
+    </message>
+    <message>
+        <location filename="../usermanager.cpp" line="228"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Действие (%1) неуспешно:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+</TS>
