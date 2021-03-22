7fa70af011373e8ff93577d48c857963f85b35cf
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-03-22 08:12:15 +0100

    Translations update from Weblate (#234)
    * weblate commits
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/ja/
    Translation: LXQt Configuration/lxqt-admin-user
    
    * weblate commits
    
    Co-authored-by: helabasa <R45XvezA@pm.me>
    
    * weblate commits
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/uk/
    Translation: LXQt Configuration/lxqt-admin-user
    
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: helabasa <R45XvezA@pm.me>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>

--- lxqt-admin-user/translations/lxqt-admin-user_si.ts
+++ lxqt-admin-user/translations/lxqt-admin-user_si.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk">
+<TS version="2.1" language="si">
 <context>
     <name>GroupDialog</name>
     <message>
         <location filename="../groupdialog.ui" line="14"/>
         <source>Group Settings</source>
-        <translation>Налаштування груп</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.ui" line="20"/>
         <source>Group name:</source>
-        <translation>Назва групи:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.ui" line="30"/>
         <source>Default</source>
-        <translation>За вибором системи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.ui" line="40"/>
         <source>Group ID:</source>
-        <translation>ID групи:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.ui" line="47"/>
         <source>Users belong to this group:</source>
-        <translation>Користувачі, що належать до цієї групи:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.cpp" line="64"/>
         <source>Error</source>
-        <translation>Помилка</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../groupdialog.cpp" line="64"/>
         <source>The group name cannot be empty.</source>
-        <translation>Назва групи не може бути порожньою.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -44,139 +44,139 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>User and Group Settings</source>
-        <translation>Налаштування користувачів і груп</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="40"/>
         <source>&amp;Users</source>
-        <translation>&amp;Користувачі</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="59"/>
         <source>Login Name</source>
-        <translation>Назва користувача</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="64"/>
         <source>User ID</source>
-        <translation>ID користувача</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="69"/>
         <source>Full Name</source>
-        <translation>Повна назва</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="74"/>
         <source>Group</source>
-        <translation>Група</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="79"/>
         <source>Home Directory</source>
-        <translation>Домашня тека</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="87"/>
         <source>Show system users (for advanced users only)</source>
-        <translation>Показати системних користувачів (тільки для досвідчених користувачів)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="95"/>
         <source>&amp;Groups</source>
-        <translation>&amp;Групи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="111"/>
         <source>Name</source>
-        <translation>Назва</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="116"/>
         <source>Group ID</source>
-        <translation>ID групи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="121"/>
         <source>Members</source>
-        <translation>Члени</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="185"/>
         <source>Edit properties of the selected item</source>
-        <translation>Змінити властивості вибраного елемента</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="158"/>
         <source>Add</source>
-        <translation>Додати</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="161"/>
         <source>Add new users or groups</source>
-        <translation>Додати нових користувачів або групи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="170"/>
         <source>Delete</source>
-        <translation>Вилучити</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="173"/>
         <source>Delete selected item</source>
-        <translation>Вилучити вибрані</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="182"/>
         <source>Properties</source>
-        <translation>Властивості</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="194"/>
         <source>Refresh</source>
-        <translation>Оновити</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="197"/>
         <source>Refresh the lists</source>
-        <translation>Оновити список</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="206"/>
         <source>Change Password</source>
-        <translation>Змінити пароль</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="209"/>
         <source>Change password for the selected user or group</source>
-        <translation>Змінити пароль для вибраного користувача або групи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="177"/>
         <location filename="../mainwindow.cpp" line="189"/>
         <location filename="../mainwindow.cpp" line="210"/>
         <source>Confirm</source>
-        <translation>Підтвердити</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="177"/>
         <source>Are you sure you want to delete the selected user?</source>
-        <translation>Ви дійсно бажаєте видалити позначеного користувача?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="189"/>
         <source>Are you sure you want to delete the selected group?</source>
-        <translation>Ви дійсно бажаєте видалити позначену групу?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="200"/>
         <source>Input the new password for %1:</source>
-        <translation>Введіть новий пароль для %1:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="210"/>
         <source>Are you sure you want to set a empty password?</source>
-        <translation>Дійсно хочете встановити порожній пароль?</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -184,72 +184,72 @@
     <message>
         <location filename="../userdialog.ui" line="14"/>
         <source>User Settings</source>
-        <translation>Налаштування користувачів</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="24"/>
         <source>General</source>
-        <translation>Загальне</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="43"/>
         <source>Full name:</source>
-        <translation>Повна назва:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="33"/>
         <source>Login name:</source>
-        <translation>Назва користувача:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="53"/>
         <source>User ID:</source>
-        <translation>ID користувача:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="60"/>
         <source>Default</source>
-        <translation>За вибором системи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="70"/>
         <source>Main group:</source>
-        <translation>Основна група:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="85"/>
         <source>Groups</source>
-        <translation>Групи</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="91"/>
         <source>The user belongs to the following groups:</source>
-        <translation>Користувач є членом наступних груп:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="109"/>
         <source>Advanced</source>
-        <translation>Більше</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="115"/>
         <source>Login shell:</source>
-        <translation>Shell логування:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.ui" line="132"/>
         <source>Home directory:</source>
-        <translation>Домашня тека:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.cpp" line="120"/>
         <source>Error</source>
-        <translation>Помилка</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../userdialog.cpp" line="120"/>
         <source>The user name cannot be empty.</source>
-        <translation>Назва користувача не може бути порожньою.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -257,12 +257,12 @@
     <message>
         <location filename="../usermanager.cpp" line="227"/>
         <source>lxqt-admin-user</source>
-        <translation>lxqt-admin-user</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../usermanager.cpp" line="228"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation>&lt;strong&gt;Операція (%1) не виконана:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
