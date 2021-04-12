17ba39624f35da996c22f17902efcc102fc32e22
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-12 12:36:53 +0200

    weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/et/
    Translation: LXQt Configuration/lxqt-admin-time

---
+++ lxqt-admin-time/translations/lxqt-admin-time_et.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Aeg:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Kuupäev:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Kasuta NTP-põhist aja sünkroniseerimist võrgust</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>Süsteemi sisemine reaalaja kell on kohalikus ajavööndis</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="122"/>
+        <source>lxqt-admin-time-helper</source>
+        <translation>lxqt-admin-time-helper</translation>
+    </message>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="123"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Toiming (%1) ei õnnestunud:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="45"/>
+        <source>Time and date configuration</source>
+        <translation>Kellaaja ja kuupäeva seadistused</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="51"/>
+        <source>Date and time</source>
+        <translation>Kuupäev ja kellaaeg</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="59"/>
+        <source>Timezone</source>
+        <translation>Ajavöönd</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="120"/>
+        <location filename="../timeadmindialog.cpp" line="130"/>
+        <location filename="../timeadmindialog.cpp" line="137"/>
+        <location filename="../timeadmindialog.cpp" line="147"/>
+        <source>Error</source>
+        <translation>Viga</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="154"/>
+        <source>Change RTC to be in localtime requires a reboot</source>
+        <translation>Süsteemi sisemise reaalaja kella seadmine kohalikku ajavööndisse eeldab taaskäivitust</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="154"/>
+        <source>Change RTC to be in UTC requires a reboot</source>
+        <translation>Süsteemi sisemise reaalaja kella seadmine UTC/GMT ajavööndisse eeldab taaskäivitust</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="155"/>
+        <source>Reboot required</source>
+        <translation>Taaskäivitus on vajalik</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Sinu praegune ajavöönd:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="32"/>
+        <source>Filter</source>
+        <translation>Otsi</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+</context>
+</TS>
