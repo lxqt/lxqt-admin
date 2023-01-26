2f9587a16e73d8bd1eab58a1c428e44247e3406b
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-01-27 01:55:45 +0800

    weblate commits (#273)
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/oc/
    Translation: LXQt Configuration/lxqt-admin-user

---
+++ lxqt-admin-user/translations/lxqt-admin-user_oc.ts
@@ -0,0 +1,268 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Paramètres del grop</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Nom del grop :</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="30"/>
+        <source>Default</source>
+        <translation>Per defaut</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="40"/>
+        <source>Group ID:</source>
+        <translation>Identificant del grop :</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="47"/>
+        <source>Users belong to this group:</source>
+        <translation>Utilizaires d’aqueste grop :</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Lo nom del grop pòt pas èsser void.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Paramètres d’utilizaires e de grops</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="40"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Utilizaires</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Login Name</source>
+        <translation>Nom d’utilizaire</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>User ID</source>
+        <translation>ID de l’utilizaire</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Full Name</source>
+        <translation>Nom complèt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Group</source>
+        <translation>Grop</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="79"/>
+        <source>Home Directory</source>
+        <translation>Repertòri personal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Afichar los utilizaires del sistèma (pels utilizaires avançats sonque)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="95"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Grops</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Group ID</source>
+        <translation>ID del grop</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Members</source>
+        <translation>Membres</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Edit properties of the selected item</source>
+        <translation>Modificar las proprietats de l’element seleccionat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>Add</source>
+        <translation>Ajustar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>Add new users or groups</source>
+        <translation>Ajustar d’utilizaires novèls o de grops novèls</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Delete</source>
+        <translation>Suprimir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="176"/>
+        <source>Delete selected item</source>
+        <translation>Suprimir l’element seleccionat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <source>Properties</source>
+        <translation>Proprietats</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="197"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="200"/>
+        <source>Refresh the lists</source>
+        <translation>Actualizar las listas</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="209"/>
+        <source>Change Password</source>
+        <translation>Cambiar lo senhal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="212"/>
+        <source>Change password for the selected user or group</source>
+        <translation>Cambiar lo senhal per l’utilizaire o lo grop seleccionat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Confirm</source>
+        <translation>Confirmar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Volètz vertadièrament suprimir l’utilizaire seleccionat ?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Volètz vertadièrament suprimir lo grop seleccionat ?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="200"/>
+        <source>Input the new password for %1:</source>
+        <translation>Picar lo senhal novèl per %1 :</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Are you sure you want to set a empty password?</source>
+        <translation>Volètz vertadièrament definir un senhal void ?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Paramètres d’utilizaire</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Full name:</source>
+        <translation>Nom complèt :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Login name:</source>
+        <translation>Identificant :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>User ID:</source>
+        <translation>ID de l’utilizaire :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="60"/>
+        <source>Default</source>
+        <translation>Per defaut</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="70"/>
+        <source>Main group:</source>
+        <translation>Grop principal :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="85"/>
+        <source>Groups</source>
+        <translation>Grops</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="91"/>
+        <source>The user belongs to the following groups:</source>
+        <translation>L’utilizaire aparten als grops seguents :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="109"/>
+        <source>Advanced</source>
+        <translation>Avançat</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="115"/>
+        <source>Login shell:</source>
+        <translation>Shell de connexion :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="132"/>
+        <source>Home directory:</source>
+        <translation>Repertòri personal :</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Lo nom d’utilizaire pòt pas èsser void.</translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../usermanager.cpp" line="227"/>
+        <source>lxqt-admin-user</source>
+        <translation>lxqt-admin-user</translation>
+    </message>
+    <message>
+        <location filename="../usermanager.cpp" line="228"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;L’action (%1) a fracassat :&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+</TS>
