f965ca5154819cceb590912377efb961acdb88b6
Weblate <noreply@weblate.org>
2023-05-26 23:11:46 +0200

    weblate commits
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/fi/
    Translation: LXQt Configuration/lxqt-admin-time
    Translation: LXQt Configuration/lxqt-admin-user

--- lxqt-admin-time/translations/lxqt-admin-time_fi.ts
+++ lxqt-admin-time/translations/lxqt-admin-time_fi.ts
@@ -21,12 +21,12 @@
     <message>
         <location filename="../datetime.ui" line="70"/>
         <source>Enable network time synchronization (NTP)</source>
-        <translation type="unfinished"></translation>
+        <translation>Päivitä kellonaika verkosta (NTP)</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="77"/>
         <source>RTC is in local time</source>
-        <translation type="unfinished"></translation>
+        <translation>Reaaliaikakello (RTC) on paikallisessa ajassa</translation>
     </message>
 </context>
 <context>
@@ -35,13 +35,13 @@
         <location filename="../fbsdtimedatectl.cpp" line="122"/>
         <location filename="../nosdtimedatectl.cpp" line="141"/>
         <source>lxqt-admin-time-helper</source>
-        <translation type="unfinished"></translation>
+        <translation>lxqt-admin-time-helper</translation>
     </message>
     <message>
         <location filename="../fbsdtimedatectl.cpp" line="123"/>
         <location filename="../nosdtimedatectl.cpp" line="142"/>
         <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;Toiminto (%1) epäonnistui:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
     </message>
 </context>
 <context>
@@ -49,17 +49,17 @@
     <message>
         <location filename="../timeadmindialog.cpp" line="45"/>
         <source>Time and date configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Aika-asetukset</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="51"/>
         <source>Date and time</source>
-        <translation type="unfinished"></translation>
+        <translation>Aika ja päivä</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="59"/>
         <source>Timezone</source>
-        <translation type="unfinished"></translation>
+        <translation>Aikavyöhyke</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="122"/>
@@ -67,22 +67,22 @@
         <location filename="../timeadmindialog.cpp" line="139"/>
         <location filename="../timeadmindialog.cpp" line="149"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Virhe</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="156"/>
         <source>Change RTC to be in localtime requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reaaliaikakellon (RTC) muuttaminen paikalliseen aikaan vaatii uudelleenkäynnistyksen</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="156"/>
         <source>Change RTC to be in UTC requires a reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Reaaliaikakellon (RTC) muuttaminen UTC-aikaan vaatii uudelleenkäynnistyksen</translation>
     </message>
     <message>
         <location filename="../timeadmindialog.cpp" line="157"/>
         <source>Reboot required</source>
-        <translation type="unfinished"></translation>
+        <translation>Uudelleenkäynnistys vaaditaan</translation>
     </message>
 </context>
 <context>
@@ -90,12 +90,12 @@
     <message>
         <location filename="../timezone.ui" line="19"/>
         <source>Your current timezone:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nykyinen aikavyöhyke:</translation>
     </message>
     <message>
         <location filename="../timezone.ui" line="32"/>
         <source>Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Suodata</translation>
     </message>
 </context>
 <context>
@@ -103,7 +103,7 @@
     <message>
         <location filename="../timezone.cpp" line="42"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>-</translation>
     </message>
 </context>
 </TS>
