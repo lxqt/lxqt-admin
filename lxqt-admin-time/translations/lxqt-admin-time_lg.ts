a4d0ae7f3478e9af5f0a6276e269e077da9fa547
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-02-06 21:13:36 +0100

    weblate commits (#290)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user-menu-entry/lg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/lg/
    Translation: LXQt Configuration/lxqt-admin-time
    Translation: LXQt Configuration/lxqt-admin-time Menu Entry
    Translation: LXQt Configuration/lxqt-admin-user
    Translation: LXQt Configuration/lxqt-admin-user Menu Entry
    
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ lxqt-admin-time/translations/lxqt-admin-time_lg.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Saawa:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Nnaku:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Tandika kukwataganya embala y&apos;obudde ku kayungirizi (NTP)</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC ekolera ku ssaawa ez&apos;omu kitundu</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="122"/>
+        <location filename="../nosdtimedatectl.cpp" line="141"/>
+        <source>lxqt-admin-time-helper</source>
+        <translation>Ekiyamba-okuteekateeka-eby&apos;okubala-obudde-ekya-LXQt</translation>
+    </message>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="123"/>
+        <location filename="../nosdtimedatectl.cpp" line="142"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Ekikolwa (%1) kiremye:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="45"/>
+        <source>Time and date configuration</source>
+        <translation>Enteekateeka za saawa n&apos;ennaku</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="51"/>
+        <source>Date and time</source>
+        <translation>Ennaku ne saawa</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="59"/>
+        <source>Timezone</source>
+        <translation>Ekifo essaawa mwe zikolera</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="122"/>
+        <location filename="../timeadmindialog.cpp" line="132"/>
+        <location filename="../timeadmindialog.cpp" line="139"/>
+        <location filename="../timeadmindialog.cpp" line="149"/>
+        <source>Error</source>
+        <translation>Kiremya</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="156"/>
+        <source>Change RTC to be in localtime requires a reboot</source>
+        <translation>RTC gikyuse ekolera ku saawa ez&apos;omu kitundu. Kyetaagisa kuddamu okukoleeza kompyuta</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="156"/>
+        <source>Change RTC to be in UTC requires a reboot</source>
+        <translation>RTC gikyuse ekolere ku UTC. Kyetaagisa kuddamu kukoleeza kompyuta</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="157"/>
+        <source>Reboot required</source>
+        <translation>Kyetaagisa kuddamu kukoleeza kompyuta</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Essaawa ez&apos;omu kitundu w&apos;oli:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="32"/>
+        <source>Filter</source>
+        <translation>Ekisunsuzibwa</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Bbule</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-admin-user/translations/lxqt-admin-user_lg.ts
@@ -0,0 +1,268 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Enteekateeka za guluupu</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Linnya lya guluupu:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="30"/>
+        <source>Default</source>
+        <translation>Ekya bulijjo</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="40"/>
+        <source>Group ID:</source>
+        <translation>Namba ya guluupu:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="47"/>
+        <source>Users belong to this group:</source>
+        <translation>Abali mu guluupu eno:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>Error</source>
+        <translation>Kiremya</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>The group name cannot be empty.</source>
+        <translation>Guluupu eteekwa okuba n&apos;erinnya.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Enteekateeka ez&apos;abakozesa ne guluupu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="40"/>
+        <source>&amp;Users</source>
+        <translation>&amp;Bakozesa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Login Name</source>
+        <translation>Linnya lya akawunti</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>User ID</source>
+        <translation>Namba ya mukozesa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Full Name</source>
+        <translation>Erinnya erijjuvu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Group</source>
+        <translation>Guluupu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="79"/>
+        <source>Home Directory</source>
+        <translation>Tterekero lya akawunti</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation>Laga ebitundu bya sisitemu ebibalibwa ng&apos;abakozesa (kyetaagisa bumanyirivu)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="95"/>
+        <source>&amp;Groups</source>
+        <translation>&amp;Guluupu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Name</source>
+        <translation>Linnya</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Group ID</source>
+        <translation>Namba ya guluupu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Members</source>
+        <translation>Abalimu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Edit properties of the selected item</source>
+        <translation>Kyusa ebikwata ku kitangaazidwa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>Add</source>
+        <translation>Yongerako</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>Add new users or groups</source>
+        <translation>Yongerako bakozesa oba guluupu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Delete</source>
+        <translation>Gyawo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="176"/>
+        <source>Delete selected item</source>
+        <translation>Gyawo ekitangaazidwa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <source>Properties</source>
+        <translation>Kyusa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="197"/>
+        <source>Refresh</source>
+        <translation>Ddamu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="200"/>
+        <source>Refresh the lists</source>
+        <translation>Ddamu okukebera nkalala</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="209"/>
+        <source>Change Password</source>
+        <translation>Kyusa akasumuluzo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="212"/>
+        <source>Change password for the selected user or group</source>
+        <translation>Kyusa akasumuluzo ak&apos;omukozesa oba guluupu ebitangaazidwa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Confirm</source>
+        <translation>Kakasa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation>Ddala oyagala okugyawo omukozesa atangaazidwa?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation>Ddala oyagala okugyawo guluupu etangaazidwa?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="200"/>
+        <source>Input the new password for %1:</source>
+        <translation>Wandika akasumuluzo ka %1 akapya:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Are you sure you want to set a empty password?</source>
+        <translation>Ddala oyagala waleme okubawo akasumuluzo?</translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Enteekateeka za mukozesa</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Eby&apos;awamu</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Full name:</source>
+        <translation>Erinnya erijjuvu:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Login name:</source>
+        <translation>Linnya lya akawunti:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>User ID:</source>
+        <translation>Namba ya mukozesa:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="60"/>
+        <source>Default</source>
+        <translation>Kya bulijjo</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="70"/>
+        <source>Main group:</source>
+        <translation>Guluupu enkulu:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="85"/>
+        <source>Groups</source>
+        <translation>Guluupu</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="91"/>
+        <source>The user belongs to the following groups:</source>
+        <translation>Omukozesa ono ali mu guluupu zino:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="109"/>
+        <source>Advanced</source>
+        <translation>Ebyetaagisa bumanyirivu</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="115"/>
+        <source>Login shell:</source>
+        <translation>Nzivvuunuzi ey&apos;akawunti:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="132"/>
+        <source>Home directory:</source>
+        <translation>Tterekero ya akawunti:</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>Error</source>
+        <translation>Kiremya</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>The user name cannot be empty.</source>
+        <translation>Omukozesa ateekwa okuba n&apos;erinnya.</translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../usermanager.cpp" line="227"/>
+        <source>lxqt-admin-user</source>
+        <translation>Kiteekateeka-bakozesa-lxqt</translation>
+    </message>
+    <message>
+        <location filename="../usermanager.cpp" line="228"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Ekikolwa (%1) kiremye:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+</TS>
