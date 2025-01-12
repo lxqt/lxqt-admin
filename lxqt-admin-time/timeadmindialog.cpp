21f51c86774cf79bcd66b200d8c382a0c928b5d9
Ian Farrell <ianfarrell47@gmail.com>
2025-01-12 06:55:27 -0500

    Use "preferences-system-time" for both

--- lxqt-admin-time/timeadmindialog.cpp
+++ lxqt-admin-time/timeadmindialog.cpp
@@ -49,8 +49,7 @@ TimeAdminDialog::TimeAdminDialog(QWidget *parent):
     mWindowTitle = windowTitle();
 
     mDateTimeWidget = new DateTimePage(mTimeDateCtl.useNtp(), mTimeDateCtl.localRtc(), this);
-    addPage(mDateTimeWidget, tr("Date and time"), QStringList() << QStringLiteral("preferences-system-time")
-        << QStringLiteral("preferences-desktop"));
+    addPage(mDateTimeWidget, tr("Date and time"), QStringLiteral("preferences-system-time"));
     connect(this, &LXQt::ConfigDialog::reset, mDateTimeWidget, &DateTimePage::reload);
     connect(mDateTimeWidget, &DateTimePage::changed, this, &TimeAdminDialog::onChanged);
 
@@ -58,7 +57,7 @@ TimeAdminDialog::TimeAdminDialog(QWidget *parent):
     QString currentZone;
     loadTimeZones(zones, currentZone);
     mTimezoneWidget = new TimezonePage(zones, currentZone, this);
-    addPage(mTimezoneWidget, tr("Timezone"), QStringLiteral("preferences-desktop"));
+    addPage(mTimezoneWidget, tr("Timezone"), QStringLiteral("preferences-system-time"));
     connect(this,&TimeAdminDialog::reset, mTimezoneWidget, &TimezonePage::reload);
     connect(mTimezoneWidget, &TimezonePage::changed, this, &TimeAdminDialog::onChanged);
 
