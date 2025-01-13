07007697d0c023adc9f408ebb07839ccf2828e46
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-13 04:15:49 +0100

    weblate commits (#303)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user-menu-entry/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user/kab/
    Translation: LXQt Configuration/lxqt-admin-time
    Translation: LXQt Configuration/lxqt-admin-time Menu Entry
    Translation: LXQt Configuration/lxqt-admin-user
    Translation: LXQt Configuration/lxqt-admin-user Menu Entry
    
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>

---
+++ lxqt-admin-time/translations/lxqt-admin-time_ka.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>დრო:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>სს:წთ:წმ</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>თარიღი:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>დროის ქსელით სინქრონიზაციის ჩართვა (NTP)</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC ლოკალურ დროშია</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../nosdtimedatectl.cpp" line="144"/>
+        <source>lxqt-admin-time-helper</source>
+        <translation>lxqt-admin-time-helper</translation>
+    </message>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="123"/>
+        <location filename="../nosdtimedatectl.cpp" line="145"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Action (%1) ჩავარდა:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="46"/>
+        <source>Time and date configuration</source>
+        <translation>დროისა და თარიღის მორგება</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="52"/>
+        <source>Date and time</source>
+        <translation>თარიღი და დრო</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="60"/>
+        <source>Timezone</source>
+        <translation>დროის სარტყელი</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="123"/>
+        <location filename="../timeadmindialog.cpp" line="133"/>
+        <location filename="../timeadmindialog.cpp" line="140"/>
+        <location filename="../timeadmindialog.cpp" line="150"/>
+        <source>Error</source>
+        <translation>შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="157"/>
+        <source>Change RTC to be in localtime requires a reboot</source>
+        <translation>RTC-ის ლოკალურ დროზე გადართვას გადატვირთვა სჭირდება</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="157"/>
+        <source>Change RTC to be in UTC requires a reboot</source>
+        <translation>RTC-ის UTC-ზე გადართვას გადატვირთვა სჭირდება</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="158"/>
+        <source>Reboot required</source>
+        <translation>აუცილებელია გადატვირთვა</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>თქვენი მიმდინარე დროის სარტყელი:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="32"/>
+        <source>Filter</source>
+        <translation>ფილტრი</translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>არცერთი</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-admin-user/translations/lxqt-admin-user_kab.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kab">
+<context>
+    <name>GroupDialog</name>
+    <message>
+        <location filename="../groupdialog.ui" line="14"/>
+        <source>Group Settings</source>
+        <translation>Iɣewwaṛen n ugraw</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="20"/>
+        <source>Group name:</source>
+        <translation>Isem n ugraw:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="30"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="40"/>
+        <source>Group ID:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.ui" line="47"/>
+        <source>Users belong to this group:</source>
+        <translation>Iseqdacen n wegraw-a:</translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../groupdialog.cpp" line="64"/>
+        <source>The group name cannot be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>User and Group Settings</source>
+        <translation>Iɣewwaṛen n yisedqacen akked d yegrawen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="40"/>
+        <source>&amp;Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Login Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>User ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Full Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Group</source>
+        <translation>Agraw</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="79"/>
+        <source>Home Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>Show system users (for advanced users only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="95"/>
+        <source>&amp;Groups</source>
+        <translation>I&amp;grawen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Name</source>
+        <translation>Isem</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Group ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Members</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Edit properties of the selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>Add</source>
+        <translation>Rnu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="164"/>
+        <source>Add new users or groups</source>
+        <translation>Rnu iseqdacen neɣ igrawen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Delete</source>
+        <translation>Kkes</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="176"/>
+        <source>Delete selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="197"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="200"/>
+        <source>Refresh the lists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="209"/>
+        <source>Change Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="212"/>
+        <source>Change password for the selected user or group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="179"/>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <location filename="../mainwindow.cpp" line="212"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="179"/>
+        <source>Are you sure you want to delete the selected user?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="191"/>
+        <source>Are you sure you want to delete the selected group?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="202"/>
+        <source>Input the new password for %1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="212"/>
+        <source>Are you sure you want to set a empty password?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserDialog</name>
+    <message>
+        <location filename="../userdialog.ui" line="14"/>
+        <source>User Settings</source>
+        <translation>Iɣewwaṛen n useqdac</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="24"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="43"/>
+        <source>Full name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="33"/>
+        <source>Login name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="53"/>
+        <source>User ID:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="60"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="70"/>
+        <source>Main group:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="85"/>
+        <source>Groups</source>
+        <translation>Igrawen</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="91"/>
+        <source>The user belongs to the following groups:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="109"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="115"/>
+        <source>Login shell:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.ui" line="132"/>
+        <source>Home directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>Error</source>
+        <translation>Tuccḍa</translation>
+    </message>
+    <message>
+        <location filename="../userdialog.cpp" line="120"/>
+        <source>The user name cannot be empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../usermanager.cpp" line="229"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
