250e8d540bf206b08eebcd13d5303ba17cf7ef34
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-08-12 18:23:56 +0200

    weblate commits (#318)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time-menu-entry/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-time/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-admin-user-menu-entry/eu/
    Translation: LXQt Configuration/lxqt-admin-time
    Translation: LXQt Configuration/lxqt-admin-time Menu Entry
    Translation: LXQt Configuration/lxqt-admin-user Menu Entry
    
    Co-authored-by: Roberalz <lumintnian@outlook.com>

---
+++ lxqt-admin-time/translations/lxqt-admin-time_eu.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eu">
+<context>
+    <name>DateTime</name>
+    <message>
+        <location filename="../datetime.ui" line="23"/>
+        <source>Time:</source>
+        <translation>Ordua:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="39"/>
+        <source>HH:mm:ss</source>
+        <translation>HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="53"/>
+        <source>Date:</source>
+        <translation>Data:</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="70"/>
+        <source>Enable network time synchronization (NTP)</source>
+        <translation>Gaitu sareko denbora sinkronizazioa (NTP)</translation>
+    </message>
+    <message>
+        <location filename="../datetime.ui" line="77"/>
+        <source>RTC is in local time</source>
+        <translation>RTC ordu lokalean dago</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../nosdtimedatectl.cpp" line="146"/>
+        <source>lxqt-admin-time-helper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fbsdtimedatectl.cpp" line="125"/>
+        <location filename="../nosdtimedatectl.cpp" line="147"/>
+        <source>&lt;strong&gt;Action (%1) failed:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</source>
+        <translation>&lt;strong&gt;Ekintzak (%1) huts egin du:&lt;/strong&gt;&lt;br/&gt;&lt;pre&gt;%2&lt;/pre&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TimeAdminDialog</name>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="46"/>
+        <source>Time and date configuration</source>
+        <translation>Ordu eta data konfigurazioa</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="52"/>
+        <source>Date and time</source>
+        <translation>Data eta ordua</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="60"/>
+        <source>Timezone</source>
+        <translation>Ordu-zona</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="123"/>
+        <location filename="../timeadmindialog.cpp" line="133"/>
+        <location filename="../timeadmindialog.cpp" line="140"/>
+        <location filename="../timeadmindialog.cpp" line="150"/>
+        <source>Error</source>
+        <translation>Errorea</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="159"/>
+        <source>Change RTC to be in localtime requires a reboot</source>
+        <translation>RTC ordu lokalean jartzeko aldaketak berrabiarazi behar da</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="160"/>
+        <source>Change RTC to be in UTC requires a reboot</source>
+        <translation>RTC UTC-n egoteko aldaketak berrabiarazi behar da</translation>
+    </message>
+    <message>
+        <location filename="../timeadmindialog.cpp" line="161"/>
+        <source>Reboot required</source>
+        <translation>Berrabiarazi behar da</translation>
+    </message>
+</context>
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.ui" line="19"/>
+        <source>Your current timezone:</source>
+        <translation>Zure uneko ordu-zona:</translation>
+    </message>
+    <message>
+        <location filename="../timezone.ui" line="32"/>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimezonePage</name>
+    <message>
+        <location filename="../timezone.cpp" line="42"/>
+        <source>None</source>
+        <translation>Bat ere ez</translation>
+    </message>
+</context>
+</TS>
